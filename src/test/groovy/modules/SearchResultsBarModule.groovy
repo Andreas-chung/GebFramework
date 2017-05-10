94ba25e1066cdbf2291a843acd4092542b737bdf
Andreas Chung <andreas.chung@rightmove.co.uk>
2017-05-10 17:38:56 +0100

    Adding more steps

--- src/test/groovy/modules/SearchResultsBarModule.groovy
+++ src/test/groovy/modules/SearchResultsBarModule.groovy
@@ -3,16 +3,17 @@ package modules
 import geb.Module
 
 
-class SearchResultsBarModule extends Module{
+class SearchResultsBarModule extends Module {
+
 
     static content = {
         sortByDropDown(wait:true) {$('#DashSortByContainer')}
-        sortByOptions(wait:true) {$('#SortMenu')}
+        sortByOptions(wait:false) {$('#SortMenu')}
     }
 
     void selectSortByOption(String option) {
-//        interact {moveToElement.(sortByDropDown)}
-        sortByOptions.click()
+        interact {moveToElement(sortByDropDown)}
+        sortByOptions.children().find { it.text() == option }.click()
     }
 
 
--- src/test/groovy/specs/SearchSpec.groovy
+++ src/test/groovy/specs/SearchSpec.groovy
@@ -15,20 +15,21 @@ class SearchSpec extends BaseSpec {
         searchBox.enterSearchText("iPhone 6s")
         searchBox.searchButtonIsClicked()
 
-        /*
+
          //this goes away after opting out ?
+
         then: "Bob reaches the new shopping experience"
         at SearchResultsNewExperiencePage
 
         when: "Bob clicks on view all search results link"
         viewAllResultsLinkIsClicked()
-*/
+
 
         then: "Bob is the search results page"
         at SearchResultsPage
 
         when: "Bob selects his sort by option"
-        searchResultsBar.selectSortByOption("Lowest")
+        searchResultsBar.selectSortByOption("Lowest price + P&P")
 
         then: "need a then"
         println "then"
